6a055e6e02a257900f23c174304292b94013dde9
wen200796 <wen200796@gmail.com>
2024-04-15 22:55:59 +0800

    0415練習

---
+++ P1_member/Controllers/PublicAgriDataController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using P1_member.Models.DTOs;
+
+namespace P1_member.Controllers
+{
+    [Route("api/PublicAgriData/[action]")]
+    [ApiController]
+    public class PublicAgriDataController : ControllerBase
+    {
+        private readonly string _base_url = "https://data.moa.gov.tw/api/v1/";
+        [HttpGet]
+        public async Task<IActionResult>GetPorkTrans(string TransDate)
+        {
+            string request_url = _base_url + "PorkTransType/?TransDate=" + TransDate;
+            var request = new HttpRequestMessage(HttpMethod.Get, request_url);
+            using (var client = new HttpClient())
+            {
+                var response = client.SendAsync(request).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var response_text = response.Content.ReadAsStringAsync().Result;
+                    JObject response_body = JObject.Parse(response_text);
+                    JArray data = (JArray)response_body["Data"];
+                    if (data == null || data.Count() ==0) 
+                    { 
+                        return NotFound();
+                    }
+                    IEnumerable<PorkTransDto> porkTransEnumerable = data.Select(item => JsonConvert.DeserializeObject<PorkTransDto>(item.ToString()));
+
+                    //var response_datas = JsonConvert.DeserializeObject<IEnumerable<PorkTransDto>>(response_text);
+                    // return Ok(response_datas);
+                    //double sumTransNum_AvgPrice = porkTransEnumerable.Sum(p => p.TransNum_AvgPrice);
+                    //return Content(data.Count().ToString());
+                    //return Content(sumTransNum_AvgPrice.ToString());
+                    PorkTransDto dailyAverage = new PorkTransDto
+                    {
+                        TransNum_AvgPrice = porkTransEnumerable.Average(p => p.TransNum_AvgPrice),
+                        SpecPig_AvgPrice = porkTransEnumerable.Average(p => p.SpecPig_AvgPrice),
+                        AvgPrice_95in_115in = porkTransEnumerable.Average(p => p.AvgPrice_95in_115in),
+                        AvgPrice_75in_95 = porkTransEnumerable.Average(p => p.AvgPrice_75in_95),
+                        AvgPrice_115up = porkTransEnumerable.Average(p => p.AvgPrice_115up),
+                        AvgPrice_75low = porkTransEnumerable.Average(p => p.AvgPrice_75low),
+                        OutPigs_AvgPrice = porkTransEnumerable.Average(p => p.OutPigs_AvgPrice),
+                        OtherPigs_AvgPrice = porkTransEnumerable.Average(p => p.OtherPigs_AvgPrice),
+                        FreezerPigs_AvgPrice = porkTransEnumerable.Average(p => p.FreezerPigs_AvgPrice),
+                        TotalTrans_ExcludeFreezer_AvgPrice = porkTransEnumerable.Average(p => p.TotalTrans_ExcludeFreezer_AvgPrice)
+                    };
+                    //return Ok(porkTransEnumerable);
+                    return Ok(dailyAverage);
+                }
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ P1_member/Models/DTOs/PorkTransDto.cs
@@ -0,0 +1,26 @@
+﻿namespace P1_member.Models.DTOs
+{
+    public class PorkTransDto
+    {
+        public  double TransNum_AvgPrice { get; set; }
+
+        public  double SpecPig_AvgPrice { get; set; }
+        
+        public  double AvgPrice_95in_115in { get; set; }
+        
+        public  double AvgPrice_75in_95 { get; set; }
+        
+        public  double AvgPrice_115up { get; set; }
+        
+        public  double AvgPrice_75low { get; set; }
+        
+        public  double OutPigs_AvgPrice { get; set; }
+        
+        public  double OtherPigs_AvgPrice { get; set; }
+        
+        public  double FreezerPigs_AvgPrice { get; set; }
+        
+        public  double TotalTrans_ExcludeFreezer_AvgPrice { get; set; }
+
+    }
+}
